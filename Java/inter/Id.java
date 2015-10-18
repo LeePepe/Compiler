db8db02770f336ddf8c9ac1f2fdcf439bf27eb36
LeePepe <1105208297@qq.com>
2015-10-18 19:37:10 +0800

    标识符

--- Java/inter/Id.java
+++ Java/inter/Id.java
@@ -3,7 +3,7 @@ import lexer.*;
 import symbols.*;
 
 public class Id extends Expr {
-    public int offset;
+    public int offset; // 相对地址
     public Id(Word id, Type p, int b) {
 	super(id, p);
 	offset = b;
